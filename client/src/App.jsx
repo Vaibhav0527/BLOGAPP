40b7a2d908fe28cf28e49729b46d11d370c46ee6
Vaibhav Gupta <vaibhav0527gupta@gmail.com>
2025-12-24 23:50:52 +0530

    add client frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
